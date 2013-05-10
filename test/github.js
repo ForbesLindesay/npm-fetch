6d0087e1c0e783e977d46d76c4643613f8671da1
Robert Kowalski <rok@kowalski.gd>
2013-05-11 00:09:21 +0200

    Tests for github
    One is telephoning to github so we might want to mock
    this one with nock or something like this in future

---
+++ test/github.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+
+var assert = require('assert')
+var downloadGithubTarball = require('../lib/github.js')
+var rimraf = require('rimraf')
+
+var dest = __dirname + '/output/foo.tar.gz'
+
+
+beforeEach(function (done) {
+  rimraf(__dirname + '/output', done);
+})
+afterEach(function (done) {
+  rimraf(__dirname + '/output', done);
+})
+
+describe('github', function () {
+  it('returns an error if the github name could not be used', function (done) {
+    downloadGithubTarball('this one will never work', dest, {}, function (err) {
+      assert.ok(err)
+      done()
+    })
+  })
+  // external
+  it('downloads archives from github', function (done) {
+    downloadGithubTarball('ForbesLindesay/npm-fetch', dest, {}, function (err) {
+      fs.exists(dest, function (exists) {
+        assert.ok(exists)
+        done()
+      })
+    })
+  })
+})
