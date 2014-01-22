d488f14337e8bf0105a3b2bfdf076f48155320bf
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-01-22 12:40:33 +0000

    Add retry logic to tests
    This should make them pass more consistently on windows

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,31 @@
 'use strict'
 
-var mkdir = require('mkdirp').sync
-var rimraf = require('rimraf').sync
+var mkdir = retry(require('mkdirp'))
+var rimraf = retry(require('rimraf'))
 
-beforeEach(function () {
-  rimraf(__dirname + '/output/')
-  mkdir(__dirname + '/output/')
+beforeEach(function (callback) {
+  rimraf(__dirname + '/output/', function (err) {
+    if (err) return callback(err)
+    mkdir(__dirname + '/output/', callback)
+  })
 })
-afterEach(function () {
-  rimraf(__dirname + '/output/')
+afterEach(function (callback) {
+  rimraf(__dirname + '/output/', callback)
 })
+
+function retry(fn) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments)
+    var cb = args.pop()
+    var attemptNo = 0
+    function attempt() {
+      fn.apply(null, args.concat([function (err, res) {
+        if (err && 4 > attemptNo++) {
+          return setTimeout(attempt, 100)
+        }
+        cb(err, res)
+      }]))
+    }
+    attempt()
+  }
+}
